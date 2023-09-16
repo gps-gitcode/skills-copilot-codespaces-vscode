dbd93a19e237cc012f13fe14e920014e3c33faa5
gps-gitcode <44733437+gps-gitcode@users.noreply.github.com>
2023-09-16 23:29:25 +0000

    Copilot thrid commit

---
+++ comments.js
@@ -0,0 +1,110 @@
+// Create web server and listen on port 3000
+// Display a message on the console
+console.log("Node server is running at port 3000");
+
+// Include the Express module
+var express = require('express');
+
+// Create an instance of Express
+var app = express();
+
+// Include the body-parser module
+var bodyParser = require('body-parser');
+
+// Configure body-parser to use JSON data
+app.use(bodyParser.json());
+
+// Configure body-parser to use URL encoded data
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// Include the Express module
+var express = require('express');
+
+// Create an instance of Express
+var app = express();
+
+// Include the body-parser module
+var bodyParser = require('body-parser');
+
+// Configure body-parser to use JSON data
+app.use(bodyParser.json());
+
+// Configure body-parser to use URL encoded data
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// Include the Express module
+var express = require('express');
+
+// Create an instance of Express
+var app = express();
+
+// Include the body-parser module
+var bodyParser = require('body-parser');
+
+// Configure body-parser to use JSON data
+app.use(bodyParser.json());
+
+// Configure body-parser to use URL encoded data
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// Include the Express module
+var express = require('express');
+
+// Create an instance of Express
+var app = express();
+
+// Include the body-parser module
+var bodyParser = require('body-parser');
+
+// Configure body-parser to use JSON data
+app.use(bodyParser.json());
+
+// Configure body-parser to use URL encoded data
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// Include the Express module
+var express = require('express');
+
+// Create an instance of Express
+var app = express();
+
+// Include the body-parser module
+var bodyParser = require('body-parser');
+
+// Configure body-parser to use JSON data
+app.use(bodyParser.json());
+
+// Configure body-parser to use URL encoded data
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// Include the Express module
+var express = require('express');
+
+// Create an instance of Express
+var app = express();
+
+// Include the body-parser module
+var bodyParser = require('body-parser');
+
+// Configure body-parser to use JSON data
+app.use(bodyParser.json());
+
+// Configure body-parser to use URL encoded data
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// Include the Express module
+var express = require('express');
+
+// Create an instance of Express
